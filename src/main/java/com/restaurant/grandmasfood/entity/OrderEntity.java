6ade44e4e4e436ff9a41186b094f75563b0766ff
sergio <sebermudezg@unal.edu.co>
2024-03-01 11:54:45 -0500

    Created all entities and validation dependency was added

--- src/main/java/com/restaurant/grandmasfood/entity/OrderEntity.java
+++ src/main/java/com/restaurant/grandmasfood/entity/OrderEntity.java
@@ -1,16 +1,84 @@
 package com.restaurant.grandmasfood.entity;
 
-import java.util.Date;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "orders")
 public class OrderEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Integer cantidad;
-    private String additionalInfo;
+
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true)
+    private UUID uuid;
+
+    @NotNull
+    @CreationTimestamp
+    private LocalDate creationDateTime;
+
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "client_document", referencedColumnName = "document")
+    @NotNull(message = "a client is required")
+    private ClientEntity client;
+
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_uuid", referencedColumnName = "uuid")
+    @NotNull(message = "a product is required")
+    private ProductEntity productEntity;
+
+    @NotNull(message = "a product quantity is required")
+    private Integer quantity;
+
+    @NotNull
+    @NotBlank(message = "extra information is required")
+    @Size(max = 511)
+    private String extraInformation;
+
+    @NotNull(message = "a subtotal is required")
     private Double subTotal;
-    private Double iva;
-    private Double total;
-    private Boolean ordered;
-    private Date dateOrdered;
-    private Date dateOrder;
+
+    @NotNull(message = "a tax is required")
+    private Double tax;
+
+    @NotNull(message = "a grand total is required")
+    private Double grandTotal;
+
+    @Column(columnDefinition = "boolean default false")
+    @NotNull
+    private Boolean delivered;
+
+    private LocalDate deliveredDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderEntity that = (OrderEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(uuid, that.uuid) && Objects.equals(creationDateTime, that.creationDateTime) && Objects.equals(client, that.client) && Objects.equals(productEntity, that.productEntity) && Objects.equals(quantity, that.quantity) && Objects.equals(extraInformation, that.extraInformation) && Objects.equals(subTotal, that.subTotal) && Objects.equals(tax, that.tax) && Objects.equals(grandTotal, that.grandTotal) && Objects.equals(delivered, that.delivered) && Objects.equals(deliveredDate, that.deliveredDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, uuid, creationDateTime, client, productEntity, quantity, extraInformation, subTotal, tax, grandTotal, delivered, deliveredDate);
+    }
 }
--- src/main/java/com/restaurant/grandmasfood/entity/ProductEntity.java
+++ src/main/java/com/restaurant/grandmasfood/entity/ProductEntity.java
@@ -1,20 +1,67 @@
 package com.restaurant.grandmasfood.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
-@Builder
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
+@Builder
+@Entity
+@Table(name = "products")
 public class ProductEntity {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String uuid;
-    private String name;
+
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true)
+    private UUID uuid;
+
+    @Column(unique = true)
+    @NotNull
+    @NotBlank(message = "a product should has a fantasy name")
+    @Size(max = 255)
+    private String fantasyName;
+
+    @Enumerated(EnumType.ORDINAL)
+    @NotNull
+    @Size(max = 255)
     private CategoryProduct category;
+
+    @NotNull
+    @NotBlank(message = "a description is required")
+    @Size(max = 511)
     private String description;
+
+    @NotNull
     private Double price;
+
+    @Column(columnDefinition =  "boolean default true")
+    @NotNull
     private Boolean available;
 
+    @OneToMany(mappedBy = "product")
+    private Set<OrderEntity> orders;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductEntity that = (ProductEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(uuid, that.uuid) && Objects.equals(fantasyName, that.fantasyName) && category == that.category && Objects.equals(description, that.description) && Objects.equals(price, that.price) && Objects.equals(available, that.available) && Objects.equals(orders, that.orders);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, uuid, fantasyName, category, description, price, available, orders);
+    }
 }
