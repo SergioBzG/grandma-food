d69c079c7176ca4af7631a72749cf429c50171ef
sergio <sebermudezg@unal.edu.co>
2024-03-07 12:12:42 -0500

    Updated all exception inherit from RuntimeException instead of Exception

--- src/main/java/com/restaurant/grandmasfood/exception/InvalidOrMissingDataException.java
+++ src/main/java/com/restaurant/grandmasfood/exception/InvalidOrMissingDataException.java
@@ -3,7 +3,7 @@ package com.restaurant.grandmasfood.exception;
 import lombok.Getter;
 
 @Getter
-public class InvalidOrMissingDataException extends Exception {
+public class InvalidOrMissingDataException extends RuntimeException {
     private final String code;
 
     public InvalidOrMissingDataException(String code, String entityName) {
