073ae66aeda4e15a82cbfebf5ff58b4838bbf511
sergio <sebermudezg@unal.edu.co>
2024-03-07 15:40:46 -0500

    Refactor dtos to be used in validations

--- src/main/java/com/restaurant/grandmasfood/validator/impl/OrderValidator.java
+++ src/main/java/com/restaurant/grandmasfood/validator/impl/OrderValidator.java
@@ -1,18 +1,26 @@
 package com.restaurant.grandmasfood.validator.impl;
 
-import com.restaurant.grandmasfood.model.Dto;
+import com.restaurant.grandmasfood.exception.InvalidOrMissingDataException;
+import com.restaurant.grandmasfood.exception.utils.ExceptionCode;
 import com.restaurant.grandmasfood.validator.IValidator;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
 
 @Component
 public class OrderValidator implements IValidator {
     @Override
-    public boolean checkMissingData(Dto dto) {
-        return false;
+    public void checkMissingData(BindingResult errors) {
+        throw new InvalidOrMissingDataException(
+                ExceptionCode.ORDER_INVALID_OR_MISSING_DATA_CODE,
+                "Order",
+                Objects.requireNonNull(errors.getFieldError()).getField()
+        );
     }
 
     @Override
-    public boolean checkFormat(String pattern) {
-        return false;
+    public void checkFormat(String pattern) {
+
     }
 }
--- src/main/java/com/restaurant/grandmasfood/validator/impl/ProductValidator.java
+++ src/main/java/com/restaurant/grandmasfood/validator/impl/ProductValidator.java
@@ -1,18 +1,27 @@
 package com.restaurant.grandmasfood.validator.impl;
 
-import com.restaurant.grandmasfood.model.Dto;
+import com.restaurant.grandmasfood.exception.InvalidOrMissingDataException;
+import com.restaurant.grandmasfood.exception.utils.ExceptionCode;
 import com.restaurant.grandmasfood.validator.IValidator;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
 
 @Component
 public class ProductValidator implements IValidator {
     @Override
-    public boolean checkMissingData(Dto dto) {
-        return false;
+    public void checkMissingData(BindingResult errors) {
+        if(errors.hasErrors())
+            throw new InvalidOrMissingDataException(
+                    ExceptionCode.PRODUCT_INVALID_OR_MISSING_DATA_CODE,
+                    "Product",
+                    Objects.requireNonNull(errors.getFieldError()).getField()
+            );
     }
 
     @Override
-    public boolean checkFormat(String pattern) {
-        return false;
+    public void checkFormat(String pattern) {
+
     }
 }
